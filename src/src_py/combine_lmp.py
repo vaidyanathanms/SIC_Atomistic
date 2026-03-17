80c2101512cdb9dbdb990d3bd94ae9905d132ef9
vms_work_desktop <vm5@ornl.gov>
2026-03-17 17:46:53 -0400

    adds the comments in the atom section of individual data file to the output file

--- src/src_py/combine_lmp.py
+++ src/src_py/combine_lmp.py
@@ -16,8 +16,8 @@ file_specs = [
 
 system = af.combine_lammps_system(file_specs, spacing=5.0)
 
-af.write_lammps_data(filename="../combined/combined_system.data", system=system,\
-                     box=((0.0, 300.0), (0.0, 300.0), (0.0, 300.0)),\
+af.write_lammps_data(filename="../../InputStructures/combined/combined_system.data", system=system,\
+                     box=((0.0, 178.5), (0.0, 178.5), (0.0, 178.5)),\
                      write_atoms='../../InputStructures/combined/all_atoms.data')
 
 print("Wrote combined_system.data")
