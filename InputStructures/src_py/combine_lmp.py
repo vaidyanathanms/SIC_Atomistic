01c9ffbede78c6636ad1b889e3eddaed502cad64
vms_work_desktop <vm5@ornl.gov>
2026-03-17 15:29:10 -0400

    updated input/output path

--- InputStructures/src_py/combine_lmp.py
+++ InputStructures/src_py/combine_lmp.py
@@ -6,12 +6,12 @@ import aux_functs as af
 # Main file for combining data files
 # one template file per species
 file_specs = [
-    ("../inp_coordfiles/lithium_large_edited.data", 1),
-    ("../inp_coordfiles/60PEO_Optimized_CH3terminated_editedterminal_edited.data", 26),
-    ("../inp_coordfiles/Li_Atom_edited.data", 1284),
-    ("../inp_coordfiles/co3_2minus_edited.data", 642),
-    ("../inp_coordfiles/V27M5_150T_edited.data", 120),
-    ("../inp_coordfiles/Li_Atom_edited.data",600)
+    ("../../InputStructures/inp_coordfiles/lithium_large_edited.data", 1),
+    ("../../InputStructures/inp_coordfiles/60PEO_Optimized_CH3terminated_editedterminal_edited.data", 26),
+    ("../../InputStructures/inp_coordfiles/Li_Atom_edited.data", 1284),
+    ("../../InputStructures/inp_coordfiles/co3_2minus_edited.data", 642),
+    ("../../InputStructures/inp_coordfiles/V27M5_150T_edited.data", 120),
+    ("../../InputStructures/inp_coordfiles/Li_Atom_edited.data",600)
 ]
 
 system = af.combine_lammps_system(file_specs, spacing=5.0)
